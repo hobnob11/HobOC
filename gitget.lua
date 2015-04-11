b4283f7a38d2901d5ef134ad9c63978522cee097
Hobnob <williamhobson62@aol.com>
2015-04-11 01:29:27 +0100

    Update gitget.lua

--- gitget.lua
+++ gitget.lua
@@ -1,5 +1,5 @@
 local shell = require("shell")
 
 local URL = "https://raw.githubusercontent.com/hobnob11/HobOC/master/"
-shell.run("wget -f "..URL.."gitget.lua")
-shell.run("wget -f "..URL.."listen.lua")
+shell.execute("wget -f "..URL.."gitget.lua")
+shell.execute("wget -f "..URL.."listen.lua")
